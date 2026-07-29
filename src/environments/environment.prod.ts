4186efdae4a8c9d960e9cdf657c094dd12a2d624
mika481 <fradjbouain@gmail.com>
2026-07-29 23:11:47 +0100

    admin controle deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: '/api/v1',
+    apiUrl: 'https://admin-controle-production.up.railway.app/api/v1',
     defaultLanguage: 'fr',
     supportedLanguages: ['fr', 'en', 'it', 'es']
 };
