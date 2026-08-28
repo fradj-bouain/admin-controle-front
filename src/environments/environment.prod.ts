fa53614b4e1e73c233b374d47c1ad4fbe7626b6a
mika481 <fradjbouain@gmail.com>
2026-08-28 19:31:46 +0100

    new env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://admin-controle-production.up.railway.app/api/v1',
+    apiUrl: 'https://admin-controle-production-6ac5.up.railway.app/api/v1',
     defaultLanguage: 'fr',
     supportedLanguages: ['fr', 'en', 'it', 'es']
 };
